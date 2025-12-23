28c20a49ee6e8013d3916c383dd253773c38d10b
hacker <prycetreasure44@gmail.com>
2025-12-24 00:43:05 +0300

    alumini6

--- main/iuiualumni/settings.py
+++ main/iuiualumni/settings.py
@@ -101,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Africa/Kampala'
 USE_I18N = True
-USE_TZ = True
+USE_TZ = False
 
 # ---------------------------
 # Static files
